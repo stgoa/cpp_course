bc38b07c97ee452c73fd3595d1270bd29e8c9b69
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-04-16 16:59:04 -0400

    added g++20 functions

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 
 #include <iostream>
 
+consteval int get_number() {
+    return 42;
+}
+
 int main() {
+    constexpr int number = get_number();
+    std::cout << "The number is " << number << std::endl;
     std::cout << "Hello World!";
     return 0;
 }
+
+// clang++-13 --version
