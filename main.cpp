ac540c37a799003f81b5af01d00df120f8ae6062
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-04-21 14:22:06 -0400

    sorted stuff from the course

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
 // Your First C++ Program
 
+// include the iostream library
 #include <iostream>
 
+// this function gets the number 42
 consteval int get_number() {
     return 42;
 }
 
-int main() {
-    constexpr int number = get_number();
-    std::cout << "The number is " << number << std::endl;
-    std::cout << "Hello World!";
-    return 0;
+// this function prints the number systems
+consteval int example() {
+     // decimal
+    int decimal = 42;
+    // binary
+    int binary = 0b101010;
+    // octal
+    int octal = 052;
+    // hexadecimal
+    int hexadecimal = 0x2A;
+
+    // print the numbers
+    std::cout << "Decimal: " << decimal << std::endl;
+    std::cout << "Binary: " << binary << std::endl;
+    std::cout << "Octal: " << octal << std::endl;
+    std::cout << "Hexadecimal: " << hexadecimal << std::endl;
+    return 42;
 }
 
-// clang++-13 --version
+
+// function that initiallizes a chat in the console
+int chat() {
+    // ask the user where he lives
+    std::string country;
+    std::cout << "Where do you live? ";
+    std::cin >> country;
+    std::cout << "I've hear that " << country<< " is very nice!" << std::endl;
+    return 42;
+}
+
+// main function
+int main() {
+    
+    return 0;
+}
